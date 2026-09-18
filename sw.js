c16710213f905ece67b64d92cc9e0e6d121296a6
15639567717ajj <131641536+15639567717ajj@users.noreply.github.com>
2026-09-18 11:54:51 +1000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'fit-note-v1';
+const CACHE = 'fit-note-v2';
 const FILES = ['./', './fit-note.html', './manifest.webmanifest', './icon.svg'];
 self.addEventListener('install', event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES))));
 self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
